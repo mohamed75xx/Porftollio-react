47b9249a1ac85683f9dae890c1e854394d9cd06f
Mohamed <achour_m@etna-alternance.net>
2023-04-10 19:17:31 +0200

    projet

---
+++ public/translations.js
@@ -0,0 +1,95 @@
+export const fr = {
+  Nav_01: "À propos",
+  Nav_02: "Expérience",
+  Nav_03: "Projets",
+  Nav_04: "Contact",
+  Nav_05: "CV",
+  About_Welcome: "HEY, JE SUIS MOHAMED",
+  About_Dev: "UN DÉVELOPPEUR FULL STACK",
+  About_Student:
+    "Je suis étudiant à l'ETNA et je suis actuellement à la recherche d'une alternance. Je suis plus à l'aise avec le développement web mais je suis ouvert à de nouvelles expériences.",
+  About_Lib: "LIBRAIRIES | FRAMEWORK",
+  About_Lang: "LANGAGES DE PROGRAMMATION",
+  About_Other: "AUTRE",
+  Background: "EXPÉRIENCE",
+  Background_2020_1: "2021",
+  Background_2020_2: "LYCÉE ÉLISA LEMONNIER",
+  Background_2020_3: "BACCALAURÉAT GÉNÉRALE",
+  Background_2020_4:
+    "Durant mes années au lycée, j'ai pu étudier Les lois de la mécanique durant mes cours de physique ainsi que la diversité du vivant lors de mes cours en S.V.T. J'ai réussi à décrocher un bac générale en spécialité physique chimie et S.V.T.",
+  Background_2022_1: "2021 - 2022",
+  Background_2022_2: "UNIVERSITÉ PARIS SORBONNE",
+  Background_2022_3: "LICENCE PHYSIQUE-CHIMIE",
+  Background_2022_4:
+    "Durant mon année de licence physique-chimie, j'ai pu développer de solides compétences en algorithmiques. J'ai également pu découvrir le Java ainsi que les bases du développement web.",
+  Background_2026_1: "2022 - 2026",
+  Background_2026_2: "ETNA",
+  Background_2026_3: "MASTER INFORMATIQUE",
+  Background_2026_4:
+    "J'ai choisi de quitter mon ancienne université pour venir étudier l'informatique à l'ETNA, en effet, la pédagogie à la SORBONNE était trop théorique pour moi. J'ai donc décidé de venir étudier à l'ETNA qui a une pédagogie bien plus axé sur la pratique.",
+  Project: "PROJETS",
+  Project_1_1: "HYRULE CASTLE",
+  Project_1_2:
+    " Création d'un mini-RPG avec des combats au tour par tour. Le projet a été réalisé en binôme en 4 jours. Nous avons pu y implémenter plusieurs fonctionnalités comme un inventaire, de la magie, différents personnages jouables, des classes et des races pour chaque personnage...",
+  Project_2_1: "TMN CARS",
+  Project_2_2:
+    " Création d'un site d'e-commerce avec React, la majorité des fonctionnalités qu'on peut retrouver sur une boutique en ligne classique y sont présent, (panier, système de connexion et d'enregistrement, filtrage des produits selon leur catégorie...) ",
+  Project_3_1: "TMN CARS API",
+  Project_3_2:
+    "Création d'une API avec toutes les fonctionnalités basiques (routes, controllers...). Utilisation de Sequelize pour la modélisation de la base de données.",
+  Project_4_1: "CODECAMP #1",
+  Project_4_2:
+    " Création d'un outil facilitant le suivi des élèves pour l'équipe pédagogique de l'ETNA. Notre projet devait principalement servir à faciliter le suivi par promotion ainsi que la prise de rendez-vous individuel pour les élèves. Utilisation de MongoDB et ExpressJS pour le back-end. Le front-end a été réalisé avec React et le framework NextJS.",
+  Contact_1: "NOM COMPLET",
+  Contact_2: "E-MAIL",
+  Contact_3: "Je vous répondrai dès que possible.",
+  Contact_4: "ENVOYER",
+};
+
+export const en = {
+  Nav_01: "About",
+  Nav_02: "Experience",
+  Nav_03: "Projects",
+  Nav_04: "Contact",
+  Nav_05: "CV",
+  About_Welcome: "HEY, I'M MOHAMED",
+  About_Dev: "A FULL STACK DEVELOPER",
+  About_Student:
+    "I'm a student at ETNA and currently looking for an apprenticeship. I'm more comfortable with web development, but I'm open to new experiences.",
+  About_Lib: "LIBRARIES | FRAMEWORK",
+  About_Lang: "PROGRAMMING LANGUAGES",
+  About_Other: "OTHER",
+  Background: "EXPERIENCE",
+  Background_2020_1: "2021",
+  Background_2020_2: "LYCÉE ÉLISA LEMONNIER",
+  Background_2020_3: "BACCALAURÉAT GÉNÉRALE",
+  Background_2020_4:
+    "During my high school years, I was able to study the laws of mechanics in my physics classes as well as the diversity of living things in my biology classes. I was able to earn a general baccalaureate with a specialization in physics, chemistry and biology.",
+  Background_2022_1: "2021 - 2022",
+  Background_2022_2: "UNIVERSITY PARIS SORBONNE",
+  Background_2022_3: "PHYSICS-CHEMISTRY DEGREE",
+  Background_2022_4:
+    "During my year of physics-chemistry degree, I was able to develop strong skills in algorithms. I also learned Java and the basics of web development.",
+  Background_2026_1: "2022 - 2026",
+  Background_2026_2: "ETNA",
+  Background_2026_3: "COMPUTER SCIENCE MASTER",
+  Background_2026_4:
+    "I chose to leave my previous university to study computer science at ETNA, because the pedagogy at SORBONNE was too theoretical for me. I decided to come to ETNA which has a pedagogy that is more focused on practice.",
+  Project: "PROJECTS",
+  Project_1_1: "HYRULE CASTLE",
+  Project_1_2:
+    "Creating a mini-RPG with turn-based combat. The project was done in pairs in 4 days. We were able to implement several features such as an inventory, magic, different playable characters, classes and races for each character...",
+  Project_2_1: "TMN CARS",
+  Project_2_2:
+    "Creating an e-commerce site with React, most of the features that can be found on a classic online store are present, (cart, login and registration system, filtering of products by category...)",
+  Project_3_1: "TMN CARS API",
+  Project_3_2:
+    "Creating an API with all the basic features (routes, controllers...). Using Sequelize for database modeling.",
+  Project_4_1: "CODECAMP #1",
+  Project_4_2:
+    "Creating a tool to facilitate student tracking for the ETNA educational team. Our project was mainly intended to facilitate tracking by promotion as well as individual appointment scheduling for students. Using MongoDB and ExpressJS for the back-end. The front-end was done with React and the NextJS framework.",
+  Contact_1: "FULL NAME",
+  Contact_2: "E-MAIL",
+  Contact_3: "I'll get back to you as soon as possible.",
+  Contact_4: "SEND"
+};
